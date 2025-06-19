6deb188e047f00e1d8f5ab553d0a324f5dd35e7f
Tayyab Wasim <affanwasim@gmail.com>
2025-06-18 22:28:54 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ export default {
 		}
 		// Only GET requests work with this proxy.
 		if (request.method !== "GET") return MethodNotAllowed(request);
-		return fetch(`https://example.com`);
+		return fetch(`https://wwdev-kxsb8b5o8-tayyabs-projects-57550584.vercel.app/`);
 	},
 } satisfies ExportedHandler;
